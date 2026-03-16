b14d046d90fea7d3194d0235aca28fcc8cc5d47e
jpadden01 <96446425+jpadden01@users.noreply.github.com>
2026-03-16 03:03:50 -0700

    Complete exercise 9

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let isNegative = a < 0 || b < 0;
+    let isNotInteger = !(Number.isInteger(a) && Number.isInteger(b));
+    if (isNegative || isNotInteger) return "ERROR";
 
+    if (a > b) {
+        const temp = a;
+        a = b;
+        b = temp;
+    }
+    
+    let sum = 0;
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
